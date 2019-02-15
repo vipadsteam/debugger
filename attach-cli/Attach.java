b30a1f1689ea87f1ec5c541dad021286f7f0bf33
xiaorui.ni <xiaorui.ni@vipshop.com>
2019-02-15 20:11:39 +0800

    release

--- attach-cli/Attach.java
+++ attach-cli/Attach.java
@@ -15,14 +15,21 @@ import com.sun.tools.attach.VirtualMachine;
  */
 public class Attach {
 
-	public static void main(String[] args) throws Exception {
-		VirtualMachine vm = null;
-		String agentjarpath = "JavaAgentAttachTest.jar"; // agentjar路径
-		String content = Files.readAllLines(Paths.get("pid.log")).get(0);// 目标JVM的进程ID（PID）
-		String url = Files.readAllLines(Paths.get("url.log")).get(0);// ip:port
-		vm = VirtualMachine.attach(content);
-		System.out.println("agentjarpath:" + agentjarpath + " url:" + url);
-		vm.loadAgent(agentjarpath, url);
-		vm.detach();
+	public static void main(String[] args) {
+		try {
+			VirtualMachine vm = null;
+			File directory = new File("");
+			String attachMainPath = directory.getAbsolutePath();
+			String agentjarpath = attachMainPath + "/JavaAgentAttachTest.jar"; // agentjar路径
+			String pid = Files.readAllLines(Paths.get("pid.log")).get(0);// 目标JVM的进程ID（PID）
+			String url = Files.readAllLines(Paths.get("url.log")).get(0);// ip:port
+			System.out.println("pid:" + pid);
+			vm = VirtualMachine.attach(pid);
+			System.out.println("agentjarpath:" + agentjarpath + " url:" + url);
+			vm.loadAgent(agentjarpath, url);
+			vm.detach();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
 	}
 }
